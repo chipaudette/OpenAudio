dba7666774dd2628906f3daf2c68c61ca5975027
Chip Audette <chipaudette@yahoo.com>
2016-10-31 13:50:00 -0400

    Teensy: NoiseTesting: do BT Classic link on Serial1

--- Arduino/Teensy Time Domain Audio/NoiseTesting/NoiseTesting.ino
+++ Arduino/Teensy Time Domain Audio/NoiseTesting/NoiseTesting.ino
@@ -4,53 +4,53 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-class AudioControlSGTL500_wStates : public AudioControlSGTL5000 {
-  public:
-    AudioControlSGTL500_wStates(void) {
-      inputSelect(AUDIO_INPUT_MIC); //also sets initial gain levels
-      volume(volume_setting);
-    }
+//class AudioControlSGTL500_wStates : public AudioControlSGTL5000 {
+//  public:
+//    AudioControlSGTL500_wStates(void) {
+//      inputSelect(AUDIO_INPUT_MIC); //also sets initial gain levels
+//      volume(volume_setting);
+//    }
+//
+//    int getLineInLevel(void) { return line_in_setting[0]; }
+//    void getLineInLevel(int vals[]) { vals[0] = line_in_setting[0]; vals[1] = line_in_setting[1]; return; };
+//    int getMicGain(void) { return mic_gain_dB; };
+//    float getVolume(void) { return volume_setting; }
+//    
+//    bool inputSelect(int n) {
+//      bool val = AudioControlSGTL5000::inputSelect(n);
+//      if (n == AUDIO_INPUT_LINEIN) {
+//        micGain(mic_gain_dB);
+//      } else if (n == AUDIO_INPUT_LINEIN) {
+//        lineInLevel(line_in_setting[0],line_in_setting[1]);
+//      }
+//      return val;
+//    }
+//    bool lineInLevel(int n) { return lineInLevel(n,n); };
+//    bool lineInLevel(int n1, int n2) {
+//      line_in_setting[0] = n1; line_in_setting[1] = n2;
+//      return AudioControlSGTL5000::lineInLevel(line_in_setting[0],line_in_setting[1]);
+//    }
+//    bool micGain(int gain_dB) {
+//      mic_gain_dB = gain_dB;
+//      return AudioControlSGTL5000::micGain(mic_gain_dB);
+//    }
+//    bool volume(float vol) {
+//      volume_setting = vol;
+//      return AudioControlSGTL5000::volume(volume_setting);
+//    }
+//  private:
+//    int mic_gain_dB = 52;
+//    int line_in_setting[2] = {5, 5};
+//    float volume_setting = 0.8;
+//};
 
-    int getLineInLevel(void) { return line_in_setting[0]; }
-    void getLineInLevel(int vals[]) { vals[0] = line_in_setting[0]; vals[1] = line_in_setting[1]; return; };
-    int getMicGain(void) { return mic_gain_dB; };
-    float getVolume(void) { return volume_setting; }
-    
-    bool inputSelect(int n) {
-      bool val = AudioControlSGTL5000::inputSelect(n);
-      if (n == AUDIO_INPUT_LINEIN) {
-        micGain(mic_gain_dB);
-      } else if (n == AUDIO_INPUT_LINEIN) {
-        lineInLevel(line_in_setting[0],line_in_setting[1]);
-      }
-      return val;
-    }
-    bool lineInLevel(int n) { return lineInLevel(n,n); };
-    bool lineInLevel(int n1, int n2) {
-      line_in_setting[0] = n1; line_in_setting[1] = n2;
-      return AudioControlSGTL5000::lineInLevel(line_in_setting[0],line_in_setting[1]);
-    }
-    bool micGain(int gain_dB) {
-      mic_gain_dB = gain_dB;
-      return AudioControlSGTL5000::micGain(mic_gain_dB);
-    }
-    bool volume(float vol) {
-      volume_setting = vol;
-      return AudioControlSGTL5000::volume(volume_setting);
-    }
-  private:
-    int mic_gain_dB = 52;
-    int line_in_setting[2] = {5, 5};
-    float volume_setting = 0.8;
-};
-
-AudioControlSGTL500_wStates     sgtl5000_1;   
+AudioControlSGTL5000    sgtl5000_1;   
 AudioInputI2S            i2s_in;          
 AudioOutputI2S           i2s_out;         
 AudioConnection          patchCord1(i2s_in, 0, i2s_out, 0);
 AudioConnection          patchCord2(i2s_in, 1, i2s_out, 1);
 
-#define DO_USB_OUT 1   //if activating this, be sure to choose USB->Audio under "Tools" in the Arduino IDE
+#define DO_USB_OUT 0   //if activating this, be sure to choose USB->Audio under "Tools" in the Arduino IDE
 #if DO_USB_OUT
   AudioOutputUSB           usb_out;   
   AudioConnection          patchCord20(i2s_in, 0, usb_out, 0);
@@ -72,12 +72,15 @@ AudioConnection          patchCord2(i2s_in, 1, i2s_out, 1);
   const int myInput = AUDIO_INPUT_LINEIN;
 #endif
 
+float headphone_val = 0.8;
 void setup() {
   // Start the serial debugging
   Serial.begin(115200);
+  Serial1.begin(9600); //115200 for BT Classic, 9600 for adafruit bluefruit BLE
   delay(500);
-  Serial.println("Teensy Aduio: NoiseTesting");
-
+  Serial.println("USB: Teensy Audio: NoiseTesting");
+  Serial1.println("BT: Teensy Audio: NoiseTesting");
+  
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
   AudioMemory(10);
@@ -85,10 +88,14 @@ void setup() {
   // Enable the audio shield and set the output volume.
   sgtl5000_1.enable();
   sgtl5000_1.inputSelect(myInput);
-  sgtl5000_1.volume(0.8); //headphone volume (max linear is about 0.85)
+  Serial.print("USB: Headphone set to : "); Serial.println(headphone_val);
+  Serial.print("BT: Headphone set to : "); Serial.println(headphone_val);
+  sgtl5000_1.volume(headphone_val); //headphone volume (max linear is about 0.85)
   //sgtl5000_1.lineInLevel(5, 5); //max is 15, default is 5
-  sgtl5000_1.adcHighPassFilterDisable();  //reduce noise?  https://forum.pjrc.com/threads/27215-24-bit-audio-boards?p=78831&viewfull=1#post78831
   
+  sgtl5000_1.adcHighPassFilterDisable();  //reduce noise?  https://forum.pjrc.com/threads/27215-24-bit-audio-boards?p=78831&viewfull=1#post78831
+  Serial.println("USB: adcHighPassFilter Disabled.");
+  Serial.println("BT: adcHighPassFilter Disabled.");
 
   //sine1.amplitude(0.9);
   //sine1.frequency(555.0);
@@ -114,7 +121,8 @@ void setLineInLevelFromPot(float val) {
     case 0:
       line_in_setting = ((int)(15.0 * val + 0.5)); //the +0.5 is so that it rounds instead of truncates
       sgtl5000_1.lineInLevel(line_in_setting, line_in_setting); //max is 15, default is 5
-      Serial.print("Line In Setting: "); Serial.println(line_in_setting);
+      Serial.print("USB: Line In Setting: "); Serial.println(line_in_setting);
+      Serial1.print("BT: Line In Setting: "); Serial1.println(line_in_setting);
       break;
     case 1:
       line_in_setting = 0;  //default
@@ -126,7 +134,8 @@ void setLineInLevelFromPot(float val) {
         line_in_setting = 5;   
       }
       sgtl5000_1.lineInLevel(line_in_setting, line_in_setting); //max is 15, default is 5
-      Serial.print("Line In Setting: "); Serial.println(line_in_setting);
+      Serial.print("USB: LineIn Setting: "); Serial.println(line_in_setting);
+      Serial1.print("BT: LineIn Setting: "); Serial1.println(line_in_setting);
       break;
   }
 }
@@ -141,7 +150,7 @@ void loop() {
   #else
     setLineInLevelFromPot(val);
   #endif
-  delay(200);
+  delay(500);
 
   // print processor and memory usage
   if (0) {
