b9337042ebd39b610c731d1aa327af9c5369fe3c
Chip Audette <chipaudette@yahoo.com>
2016-11-30 14:07:10 -0500

    BasicCompressor: more work on mfile

--- Arduino/Teensy Time Domain Audio/BasicCompressor_Float/basicCompressor.m
+++ Arduino/Teensy Time Domain Audio/BasicCompressor_Float/basicCompressor.m
@@ -1,9 +1,25 @@
 
+%https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4172289/
+
+
 fs_Hz = 44100;      %sample rate
-attack_sec = 0.01;  %doesn't end up being exact.  Error is dependent upon release time, too.
-release_sec = 0.1;  %doesn't end up being exact.  Error is dependent upon attack time, too.
+attack_sec = 0.005;  %doesn't end up being exact.  Error is dependent upon compression ratio
+release_sec = 0.2;  %doesn't end up being exact.  Error is dependent upon compression ratio
 thresh_dBFS = -10;  %threshold for compression to start
 comp_ratio = 5;     %compression ratio.  "5" means 5:1
+
+if (1)
+    %scale to make more correct
+    if (1)
+        %gain processing in linear space
+        attack_sec = attack_sec * 0.555;   %for comp_ratio = 5, thresh = -10
+        release_sec = release_sec * 0.92;   %for comp_ratio = 5, thresh = -10
+    else
+        %gain processing in power space
+        attack_sec = attack_sec * 0.01/0.02302;   %for comp_ratio = 5
+        release_sec = release_sec * 0.1 / 0.08435;   %for comp_ratio = 5
+    end
+end
 
 %generate test signal
 t_sec = ([1:4*fs_Hz]-1)/fs_Hz;
@@ -11,11 +27,11 @@ freq_Hz = 2000;  %frequency of my test signal
 wav = sqrt(2)*sin(2*pi*freq_Hz*t_sec);  %rms of 1.0 (aka 0 dB)
 
 %make step change in amplitude
-flag_isAttackTest = 0;  %do an attack or a release test?
+flag_isAttackTest = 1;  %do an attack or a release test?
 if (flag_isAttackTest)
-    levels_dBFS = [-30 0];%two amplitude levels
+    levels_dBFS = [-25 0];%two amplitude levels
 else
-    levels_dBFS = [0 -30];  %two amplitude levels
+    levels_dBFS = [0 -25];  %two amplitude levels
 end
 t_bound_sec = 1;        %time of transition
 I=find(t_sec < t_bound_sec);
@@ -28,19 +44,25 @@ wav(I) = sqrt(10.^(0.1*levels_dBFS(2)))*wav(I);
 %get signal power
 wav_pow = wav.^2;
 
-%smooth via attack and release
-attack_const = exp(-1/(attack_sec*fs_Hz));
-release_const = exp(-1/(release_sec*fs_Hz));
-new_wav_pow = ones(size(wav_pow)); new_wav_pow(1) = wav_pow(1);
-for I=2:length(wav_pow)
-    if (wav_pow(I) > wav_pow(I-1))
-        %attack
-        new_wav_pow(I) = new_wav_pow(I-1)*attack_const + wav_pow(I)*(1-attack_const);
-    else
-        %release
-        new_wav_pow(I) = new_wav_pow(I-1)*release_const + wav_pow(I)*(1-release_const);
-    end
-end
+%extract signal envlope
+env_time_const = 1e-3;
+b = 1-exp(-1/(env_time_const*fs_Hz));
+a = [1 -exp(-1/(env_time_const*fs_Hz))];
+new_wav_pow = filter(b,a,wav_pow);
+
+% %smooth wav power via attack and release
+% attack_const = exp(-1/(attack_sec*fs_Hz));
+% release_const = exp(-1/(release_sec*fs_Hz));
+% new_wav_pow = ones(size(wav_pow)); new_wav_pow(1) = wav_pow(1);
+% for I=2:length(wav_pow)
+%     if (wav_pow(I) > wav_pow(I-1))
+%         %attack
+%         new_wav_pow(I) = new_wav_pow(I-1)*attack_const + wav_pow(I)*(1-attack_const);
+%     else
+%         %release
+%         new_wav_pow(I) = new_wav_pow(I-1)*release_const + wav_pow(I)*(1-release_const);
+%     end
+% end
 
 %get power relative to threshold
 thresh_pow_FS = 10.^(0.1*thresh_dBFS);
@@ -51,25 +73,62 @@ wav_pow_rel_thresh = new_wav_pow ./ thresh_pow_FS;
 gain_pow = ones(size(wav_pow_rel_thresh));
 I=find(wav_pow_rel_thresh > 1.0);
 gain_pow(I) = 10.^((1/comp_ratio - 1)*log10(wav_pow_rel_thresh(I)));
+gain = sqrt(gain_pow);
 
-    
+%smooth gain via attack and release
+attack_const = exp(-1/(attack_sec*fs_Hz));
+release_const = exp(-1/(release_sec*fs_Hz));
+if (0)
+    %power space
+    new_gain_pow = gain_pow;
+    for I=2:length(gain)
+        if (gain_pow(I) < new_gain_pow(I-1))
+            %attack
+            new_gain_pow(I) = new_gain_pow(I-1)*attack_const + gain_pow(I)*(1-attack_const);
+        else
+            %release
+            new_gain_pow(I) = new_gain_pow(I-1)*release_const + gain_pow(I)*(1-release_const);
+        end
+    end
+    %new_gain_pow = new_gain.^2;
+    new_gain = sqrt(new_gain_pow);
+else
+    %linear space
+    new_gain = gain;
+    for I=2:length(gain)
+        if (gain(I) < new_gain(I-1))
+            %attack
+            new_gain(I) = new_gain(I-1)*attack_const + gain(I)*(1-attack_const);
+        else
+            %release
+            new_gain(I) = new_gain(I-1)*release_const + gain(I)*(1-release_const);
+        end
+    end
+    new_gain_pow = new_gain.^2;
+end
+
+%rename
+raw_gain_pow = gain_pow;
+gain_pow = new_gain_pow;
+
+new_wav = wav.*sqrt(gain_pow);
 
 %% plots
-figure;try;setFigureTallerWide;catch;end
+figure;try;setFigureTallestWide;catch;end
 ax=[];
 
-subplot(3,2,1);
+subplot(4,2,1);
 plot(t_sec,wav);
 xlabel('Time (sec)');
 ylabel('Raw Waveform');
-ylim([-1 1]);
+ylim([-2 2]);
 hold on; 
 plot(xlim,sqrt(10.^(0.1*thresh_dBFS))*[1 1],'k--','linewidth',2);
 plot(xlim,-sqrt(10.^(0.1*thresh_dBFS))*[1 1],'k--','linewidth',2);
 hold off
 ax(end+1)=gca;
 
-subplot(3,2,2);
+subplot(4,2,2);
 plot(t_sec,10*log10(wav_pow));
 xlabel('Time (sec)');
 ylabel('Signal Power (dBFS)');
@@ -79,7 +138,7 @@ plot(xlim,thresh_dBFS*[1 1],'k--','linewidth',2);
 hold off
 ax(end+1)=gca;
 
-subplot(3,2,3);
+subplot(4,2,3);
 plot(t_sec,10*log10(new_wav_pow));
 xlabel('Time (sec)');
 ylabel('Smoothed Signal Power (dBFS)');
@@ -90,7 +149,7 @@ hold off
 ax(end+1)=gca;
 
 
-subplot(3,2,4);
+subplot(4,2,4);
 plot(t_sec,10*log10(wav_pow_rel_thresh));
 xlabel('Time (sec)');
 ylabel('Signal Power (dB) Rel Threshold');
@@ -100,8 +159,8 @@ plot(xlim,[0 0],'k--','linewidth',2);
 hold off
 ax(end+1)=gca;
 
-subplot(3,2,5);
-plot(t_sec,10*log10(gain_pow));
+subplot(4,2,5);
+plot(t_sec,10*log10([raw_gain_pow(:) gain_pow(:)]));
 xlabel('Time (sec)');
 ylabel('Target Gain (dB)');
 ylim([-12 0]);
@@ -110,8 +169,20 @@ plot(xlim,[0 0],'k--','linewidth',2);
 hold off
 ax(end+1)=gca;
 
+subplot(4,2,7);
+plot(t_sec,new_wav);
+xlabel('Time (sec)');
+ylabel('New Waveform');
+ylim([-2 2]);
+hold on; 
+plot(xlim,sqrt(10.^(0.1*thresh_dBFS))*[1 1],'k--','linewidth',2);
+plot(xlim,-sqrt(10.^(0.1*thresh_dBFS))*[1 1],'k--','linewidth',2);
+hold off
+ax(end+1)=gca;
+
+
 linkaxes(ax,'x');
-xlim([0 2]);
+xlim([0.9 1.2]);
 
 % subplot(3,2,5);
 % plot(t_sec,10*log10(new_gain_pow));
@@ -122,7 +193,7 @@ xlim([0 2]);
 % plot(xlim,[0 0],'k--','linewidth',2);
 % hold off
 
-subplot(3,2,6);
+subplot(4,2,6);
 dt_sec = t_sec-t_bound_sec;
 gain_rel_final_dB = 10*log10(gain_pow/gain_pow(end));
 semilogx(dt_sec,gain_rel_final_dB)
